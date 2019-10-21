dd795dffaad8243788ffe48353f8f40c0426c14b
dmitrap <dmitrap@gmail.com>
2019-10-21 15:55:04 +0700

    project commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import s from './Navbar.module.css';
 console.log(s);
-// let s = {
-//     'nav': 'Navbar_nav__3ou9Q',
-//     'item': 'Navbar_item__3qaF3',
-//     'active' : 'Baksndakdn_actve'
- // }
 
 let c1 = "item";
 let c2 = "active";
-// "item active"
 let classes = c1 + " " + c2;
 let classesNew = `${s.item} ${c2}`;
 
@@ -32,6 +26,6 @@ const Navbar = () => {
             <a>Settings</a>
         </div>
     </nav>
-};
+}
 
 export default Navbar;
