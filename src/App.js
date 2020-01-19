ad8d1fc2d8743d53a97283103a213822c6388ed7
dmitrap <dmitrap@gmail.com>
2020-01-19 18:26:02 +0500

    axios and router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Profile from './components/Profile';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import {Route} from "react-router-dom";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import UsersContainer from "./components/Users/UsersContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
 
 const App = () => {
-  return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>);
-};
+    return (
+            <div className='app-wrapper'>
+                <Header />
+                <Navbar />
+                <div className='app-wrapper-content'>
+                    <Route path='/dialogs'
+                           render={ () => <DialogsContainer /> }/>
+
+                    <Route path='/profile/:userId?'
+                           render={ () => <ProfileContainer /> }/>
+
+                    <Route path='/users'
+                           render={ () => <UsersContainer /> }/>
+
+
+                </div>
+            </div>
+        )
+}
 
 export default App;
